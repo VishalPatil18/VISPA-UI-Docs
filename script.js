a54488944a69cb3be1cd0db41103a300a94590d2
VishalPatil18 <vishalpatil.imp@gmail.com>
2022-02-05 00:39:42 +0530

    FIX: made the site responsive and updated the landing page

---
+++ script.js
@@ -0,0 +1,21 @@
+const menuBtn = document.getElementById("menu-btn");
+const sidebarContainer = document.querySelector(".maincontent__sidebar--left");
+const sidebarEl = document.getElementById("sidebar-el");
+const sidebarCloseBtn = document.getElementById("sidebar-close-btn");
+
+menuBtn.addEventListener("click", () => {
+  sidebarContainer.style.display = "block";
+  sidebarEl.style.display = "block";
+});
+
+sidebarCloseBtn.addEventListener("click", () => {
+  sidebarContainer.style.display = "none";
+  sidebarEl.style.display = "none";
+})
+
+window.addEventListener("click", e => {
+  if (e.target == sidebarContainer) {
+    sidebarEl.style.display = "none";
+    sidebarContainer.style.display = "none";
+  }
+});
